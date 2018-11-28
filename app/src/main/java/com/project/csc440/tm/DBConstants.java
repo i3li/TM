e2486aa8a203ba4b4897ad1e40fdc24febc79eff
Ali Za <i3li.xz@gmail.com>
2018-11-29 01:44:53 +0300

    Add DBConstants class

---
+++ app/src/main/java/com/project/csc440/tm/DBConstants.java
@@ -0,0 +1,12 @@
+package com.project.csc440.tm;
+
+/**
+ * This class encapsulates all constants that are related to the database
+ */
+
+final class DBConstants {
+
+    static final String groupsPath = "groups";
+    static final String usersPath = "users";
+
+}
