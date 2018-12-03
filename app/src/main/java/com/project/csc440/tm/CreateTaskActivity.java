35df8f70b2f611538b6a328291e008720fd7d248
Ali Za <i3li.xz@gmail.com>
2018-12-03 23:00:00 +0300

    Link CreateTaskActivity with its UI

--- app/src/main/java/com/project/csc440/tm/CreateTaskActivity.java
+++ app/src/main/java/com/project/csc440/tm/CreateTaskActivity.java
@@ -2,12 +2,35 @@ package com.project.csc440.tm;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.EditText;
 
-public class CreateTaskActivity extends AppCompatActivity {
+import com.github.florent37.singledateandtimepicker.SingleDateAndTimePicker;
+
+import java.util.Date;
+
+public class CreateTaskActivity extends TMActivity {
+
+    private EditText nameEditText, detailsEditText;
+    private SingleDateAndTimePicker dateAndTimePicker;
+
+    @Override
+    int getLayoutResource() {
+        return R.layout.activity_create_task;
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_create_task);
+        setupViews();
     }
+
+    private void setupViews() {
+        dateAndTimePicker = findViewById(R.id.sdtp_due_date);
+        Date now = new Date();
+        dateAndTimePicker.setDefaultDate(now);
+        dateAndTimePicker.setMinDate(now);
+        nameEditText = findViewById(R.id.et_task_name);
+        detailsEditText = findViewById(R.id.et_task_details);
+    }
+
 }
