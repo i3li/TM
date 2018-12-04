8872883388b149cf3de498264ea73b1371e41d09
Ali Za <i3li.xz@gmail.com>
2018-12-04 03:07:50 +0300

    ViewGroupActivity ui

---
+++ app/src/main/java/com/project/csc440/tm/ViewGroupActivity.java
@@ -0,0 +1,17 @@
+package com.project.csc440.tm;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ViewGroupActivity extends TMActivity {
+
+    @Override
+    int getLayoutResource() {
+        return R.layout.activity_view_group;
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
