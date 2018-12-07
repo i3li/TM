fe5104f3bf2fff6d3e0f1b39455cd794d56e5faf
Ali Za <i3li.xz@gmail.com>
2018-12-07 16:17:09 +0300

    Listen for network status changes and update activities title to indicate the no network state

--- app/src/main/java/com/project/csc440/tm/TMActivity.java
+++ app/src/main/java/com/project/csc440/tm/TMActivity.java
@@ -1,5 +1,8 @@
 package com.project.csc440.tm;
 
+import android.content.Context;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.os.Bundle;
 import android.support.annotation.IdRes;
 import android.support.annotation.LayoutRes;
@@ -9,6 +12,9 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
 
+import java.util.Timer;
+import java.util.TimerTask;
+
 public abstract class TMActivity extends AppCompatActivity {
 
     private static final String TAG = "TMActivity";
@@ -17,10 +23,20 @@ public abstract class TMActivity extends AppCompatActivity {
 
     private Toolbar toolbar;
 
+    private Timer connectionCheckerTimer;
+    private Timer disconnectionThreeDotTimer;
+
+    private String origTitle;
+    private String waitingDots = "";
+    private boolean isConnected = false;
+    private ConnectivityManager connectivityManager;
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(getLayoutResource());
+        connectivityManager = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
+        origTitle = getTitle().toString();
         toolbar = findViewById(R.id.tb_main);
         if (toolbar != null) {
             setSupportActionBar(toolbar);
@@ -30,9 +46,91 @@ public abstract class TMActivity extends AppCompatActivity {
     }
 
     @Override
+    protected void onResume() {
+        super.onResume();
+        setupConnectionCheckerTimer();
+        isConnected = isConnected();
+        if (!isConnected)
+            onDisconnection();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        connectionCheckerTimer.cancel();
+        connectionCheckerTimer = null;
+        if (disconnectionThreeDotTimer != null) {
+            disconnectionThreeDotTimer.cancel();
+            disconnectionThreeDotTimer = null;
+        }
+    }
+
+    @Override
     public boolean onSupportNavigateUp() {
         onBackPressed();
         return true;
+    }
+
+    private boolean isConnected() {
+        NetworkInfo networkInfo = connectivityManager.getActiveNetworkInfo();
+        return networkInfo != null && networkInfo.isConnected();
+    }
+
+    private void setupConnectionCheckerTimer() {
+        connectionCheckerTimer = new Timer();
+        connectionCheckerTimer.schedule(new TimerTask() {
+            @Override
+            public void run() {
+                boolean _connected = isConnected();
+                if (_connected != isConnected) {
+                    isConnected = _connected;
+                    runOnUiThread(new Runnable() {
+                        @Override
+                        public void run() {
+                            if (isConnected)
+                                onConnection();
+                            else
+                                onDisconnection();
+                        }
+                    });
+                }
+            }
+        }, 0, 5 * 1000);
+    }
+
+    private void setupDisconnectionThreeDotTimer() {
+        disconnectionThreeDotTimer = new Timer();
+        disconnectionThreeDotTimer.schedule(new TimerTask() {
+            @Override
+            public void run() {
+                runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        String newTitle = getString(R.string.no_connection_tilte);
+                        if (waitingDots.length() == 3)
+                            waitingDots = "";
+                        else
+                            waitingDots += ".";
+                        newTitle += waitingDots;
+                        setTitle(newTitle);
+                    }
+                });
+            }
+        }, 0, 1 * 1000);
+    }
+
+    protected void onConnection() {
+        setTitle(origTitle);
+        if (disconnectionThreeDotTimer != null) {
+            disconnectionThreeDotTimer.cancel();
+            disconnectionThreeDotTimer = null;
+        }
+    }
+
+    protected void onDisconnection() {
+        setTitle(R.string.no_connection_tilte);
+        if (disconnectionThreeDotTimer == null)
+            setupDisconnectionThreeDotTimer();
     }
 
 }
